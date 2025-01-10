72a8b50a75d059eaf809ff59ba2418d0018309d8
Leyla0711 <lsuleymanova.ns@gmail.com>
2025-01-10 12:34:45 +0000

    error correction 2

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def add_tuple(tuple_a=(), tuple_b=()):
     tuple_c = (0, 0)
     if len(tuple_a) < 2:
@@ -8,5 +7,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_b = tuple_b + tuple_c
     value1 = tuple_a[0] + tuple_b[0]
     value2 = tuple_a[1] + tuple_b[1]
-    tuple_d = (value1, value2) 
+    tuple_d = (value1, value2)
     return tuple_d
