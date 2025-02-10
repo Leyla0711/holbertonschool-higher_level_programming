1c16211527fd7746e78217a94c68b3fa68aa12d7
Leyla0711 <lsuleymanova.ns@gmail.com>
2025-02-10 12:39:36 +0000

    anwesrs

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,26 +1,12 @@
 #!/usr/bin/python3
+
 """
-Module that creates a read_lines function
+Appends text to a file and returns the number of characters added.
 """
 
-
-def read_lines(filename="", nb_lines=0):
-    """[summary]
-
-    Args:
-        filename (str, optional): [description]. Defaults to "".
-        nb_lines (int, optional): [description]. Defaults to 0.
+def append_write(filename="", text=""):
     """
-    with open(filename, 'r', encoding="utf-8") as file:
-        n_lines = 0
-        for lines in file:
-            n_lines += 1
-
-        file.seek(0)
-        if nb_lines <= 0 or nb_lines >= n_lines:
-            file_data = file.read()
-            print(file_data, end="")
-        else:
-            for line in range(nb_lines):
-                file_data = file.readline()
-                print(file_data, end='')
+    Appends text to a file and returns the number of characters added.
+    """
+    with open(filename, "a", encoding="utf-8") as file:
+        return file.write(text)
