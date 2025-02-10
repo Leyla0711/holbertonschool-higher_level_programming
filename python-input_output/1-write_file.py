e4684fd53d9f4f8568fccd525b7cc2f6171cb934
Leyla0711 <lsuleymanova.ns@gmail.com>
2025-02-10 12:15:35 +0000

    error correction 2

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 """
-    Module that creates a fucntion number_of_lines
+    Module that creates a function number_of_lines
 """
 
 
 def number_of_lines(filename=""):
     """
-        Function that count the number of lines from a file
+        Function that counts the number of lines from a file
 
     Args:
-        filename (str, optional): file to be read . Defaults to "".
+        filename (str, optional): file to be read. Defaults to "".
 
     Returns:
         int : Number of lines
     """
     n_lines = 0
     with open(filename, 'r') as file:
-        for lines in file:
+        for line in file:
             n_lines += 1
-    return(n_lines)
+    return n_lines
+
