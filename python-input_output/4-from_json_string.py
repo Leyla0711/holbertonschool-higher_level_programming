d1b26a190d575e33503e8739ec06f676173bd488
Leyla0711 <lsuleymanova.ns@gmail.com>
2025-02-10 12:59:48 +0000

    answears

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
+"""Defines a JSON-to-object function."""
 import json
 
+
 def from_json_string(my_str):
-    """Returns a Python object represented by a JSON string.
-
-    Args:
-        my_str: A string containing a JSON object.
-
-    Returns:
-        A Python object represented by the JSON string.
-
-    Example:
-        >>> from_json_string('[1, 2, 3]')
-        [1, 2, 3]
-    """
+    """Return the Python object representation of a JSON string."""
     return json.loads(my_str)
